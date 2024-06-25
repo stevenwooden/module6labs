f8752159cd7747b391a432fc2f114e0ae336d44b
stevenwooden <161550247+stevenwooden@users.noreply.github.com>
2024-06-24 22:35:57 -0400

    completed calcualtor

---
+++ calculator/src/App.jsx
@@ -0,0 +1,14 @@
+import Calculator from '../Calculator'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Calculator/>
+      </div>
+    </>
+  )
+}
+
+export default App
